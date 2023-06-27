16e66e584ef293df2ea13deeaed11229272eda38
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-06-27 10:11:02 +0200

    task: update project to TypeScript

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ApplicationProvider } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </ApplicationProvider>
     </View>
   );
 }
