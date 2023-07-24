e279beedd6c08d904613aa686c26183526a594b6
AZYxV/Nathan <nschpro@gmail.com>
2023-07-24 16:18:13 +0200

    (feat):style checkout

--- components/CheckoutComponent.tsx
+++ components/CheckoutComponent.tsx
@@ -11,29 +11,55 @@ const CheckoutComponent = ({ setStep, getDataRoom }: { setStep: React.Dispatch<R
         setStep('liste');
     }
 
+    const [formules, setFormules] = useState([]);
     const [options, setOptions] = useState([]);
 
     useEffect(() => {
         getProductsByCategory(2).then((res) => {
+            const formulesArray = Object.values(res);
+            setFormules(formulesArray);
+        });
+
+        getProductsByCategory(3).then((res) => {
             const optionsArray = Object.values(res);
             setOptions(optionsArray);
-        });
+        })
     }, []);
 
-    const [checkedStates, setCheckedStates] = useState({});
-    const [totalPrice, setTotalPrice] = useState(0); // Initialize totalPrice as a number
+    // Etats pour les formules et leurs prix sélectionnés
+    const [checkedStatesFormules, setCheckedStatesFormules] = useState({});
+    const [totalPriceFormules, setTotalPriceFormules] = useState(0);
 
-    const onCheckedChange = (index: number, isChecked: boolean, price: number): void => {
-        setCheckedStates((prevState) => ({
+    // Etats pour les options et leurs prix sélectionnés
+    const [checkedStatesOptions, setCheckedStatesOptions] = useState({});
+    const [totalPriceOptions, setTotalPriceOptions] = useState(0);
+
+    const onFormuleCheckedChange = (index: number, isChecked: boolean, price: number): void => {
+        setCheckedStatesFormules((prevState) => ({
             ...prevState,
             [index]: isChecked,
         }));
 
-        setTotalPrice((prevTotalPrice) => {
+        setTotalPriceFormules((prevTotalPrice) => {
             if (isChecked) {
-                return prevTotalPrice + Number(price); // Convert price to number before addition
+                return prevTotalPrice + Number(price);
             } else {
-                return prevTotalPrice - Number(price); // Convert price to number before subtraction
+                return prevTotalPrice - Number(price);
+            }
+        });
+    };
+
+    const onOptionCheckedChange = (index: number, isChecked: boolean, price: number): void => {
+        setCheckedStatesOptions((prevState) => ({
+            ...prevState,
+            [index]: isChecked,
+        }));
+
+        setTotalPriceOptions((prevTotalPrice) => {
+            if (isChecked) {
+                return prevTotalPrice + Number(price);
+            } else {
+                return prevTotalPrice - Number(price);
             }
         });
     };
@@ -42,26 +68,40 @@ const CheckoutComponent = ({ setStep, getDataRoom }: { setStep: React.Dispatch<R
         <View style={styles.container}>
             <Button style={styles.backButton} status='danger' onPress={handleBack}>Retour</Button>
             <Card>
-                <Text>Options de séjour</Text>
-                {options.map((data, index) => (
-                    <View key={index}>
-                        <Text>{data.name} {data.price}</Text>
-                        <Toggle
-                            checked={checkedStates[index] || false}
-                            onChange={(isChecked) => onCheckedChange(index, isChecked, data.price)}
-                        >
-                            {`Checked: ${checkedStates[index] || false}`}
-                        </Toggle>
-                    </View>
-                ))}
+                <Text style={styles.title}>Formule de séjour</Text>
+                <View style={styles.gapSupplements}>
+                    {formules.map((data, index) => (
+                        <View style={styles.supplements} key={index}>
+                            <Toggle
+                                status='success'
+                                checked={checkedStatesFormules[index] || false}
+                                onChange={(isChecked) => onFormuleCheckedChange(index, isChecked, data.price)}
+                            ></Toggle>
+                            <Text>{data.name} {data.price}</Text>
+                        </View>
+                    ))}
+                </View>
+                <Text style={styles.title}>Options</Text>
+                <View style={styles.gapSupplements}>
+                    {options.map((data,index) => (
+                        <View style={styles.supplements} key={index}>
+                            <Toggle
+                                status='success'
+                                checked={checkedStatesOptions[index] || false}
+                                onChange={(isChecked) => onOptionCheckedChange(index, isChecked, data.price)}
+                            ></Toggle>
+                            <Text>{data.name} {data.price}</Text>
+                        </View>
+                    ))}
+                </View>
+                <Text style={styles.title}>Recapitulatif de votre réservation</Text>
+                <Text style={styles.totalHT}>Chambre {getDataRoom.name}</Text>
+                <Text style={styles.totalHT}>Total HT chambre : {getDataRoom.price} €</Text>
+                <Text style={styles.totalHT}>Total HT formules : {totalPriceFormules} €</Text>
+                <Text style={styles.totalHT}>Total HT options : {totalPriceOptions} €</Text>
+                <Text style={styles.totalTTC}>Total HTT : {(getDataRoom.price + totalPriceFormules + totalPriceOptions) * 1.25} €</Text>
+                <Button style={styles.buttons} status='success'>Finaliser la réservation</Button>
             </Card>
-            <Card>
-                <Text>Recapitulatif de votre réservation</Text>
-                <Text>Chambre: {getDataRoom.name}</Text>
-                <Text>Total HT: {getDataRoom.price + totalPrice} €</Text>
-                <Text>Total HTT: {(getDataRoom.price + totalPrice)*1.25} €</Text>
-            </Card>
-            <Button status='success'>Finaliser la réservation</Button>
         </View>
     );
 };
@@ -74,6 +114,41 @@ const styles = StyleSheet.create({
     },
     backButton: {
         justifyContent: 'flex-start',
+        marginBottom: 12,
+        marginTop: 12
+    },
+
+    buttons: {
+        marginBottom: 12,
+        marginTop: 12
+    },
+
+    title: {
+        textAlign: "center",
+        textTransform: "uppercase",
+        marginBottom: 12,
+        marginTop: 12
+    },
+
+    supplements: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        gap: 25
+    },
+
+    gapSupplements: {
+        gap: 10
+    },
+
+    totalHT: {
+        marginBottom: 8,
+    },
+
+    totalTTC: {
+        fontWeight: "bold",
+        fontSize: 20,
+        textTransform: "uppercase"
     }
 });
 
