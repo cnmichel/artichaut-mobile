0f7108e02589974a3aa0170b27c4c98263452e9a
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-07-27 09:53:59 +0200

    update profil page

--- Styles/UserProfile.style.ts
+++ Styles/UserProfile.style.ts
@@ -1,129 +1,127 @@
 import { StyleSheet } from "react-native";
-import {components} from "@eva-design/eva/mapping";
-import height = components.Avatar.meta.parameters.height;
+import { components } from "@eva-design/eva/mapping";
+
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1 ,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        gap: 16,
-        height: "100%"
-    },
-    
-    Background: {
-        flex: 1,
-        resizeMode: 'cover',
-        width: '100%',
-        height: '100%',
-        alignItems: "center",
-        borderRadius: 10,
-        
-    },
-    
-    header: {
-        borderRadius: 10,
-        padding: 3,
-        flex: 1 ,
-        flexDirection: "column",
-    },
-    
-    user: {
-        textAlign: 'left',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    
-    img: {
-        height:20,
-        width:20,
-    },
-    
-    title: {
-        marginTop: 20,
-        textAlign: 'left',
-        fontSize: 30,
-        fontWeight: "bold",
-        marginBottom:10,
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
 
-    },
-    
-    infos: {
-        marginTop: 28,
-        textAlign: 'left',
-        fontSize: 20,
-        fontWeight: "bold",
-        marginBottom:10,
+  background: {
+    width: "100%",
+    height: 150,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
 
-    },
-    
-    form: {
-        width: '90%',
-        flex: 1 ,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        marginBottom: 100,
-    },
-    
-    editNames: {
-        flex: 1 ,
-        flexDirection: "row",
-    },
-    
-    editInfos: {
-        flex: 1 ,
-        flexDirection: "row",
-        marginTop: 20,
-        marginBottom: 20,
-    },
-    
-    other: {
-        flex: 1 ,
-        flexDirection: "column",
-    },
-    
-    names: {
-        height: 40,
-        borderColor: "gray",
-        width: 200,
-        borderWidth: 1,
-        borderRadius: 5,
-        paddingHorizontal: 10,
-        backgroundColor: "white",
-    },
-    
-    input: {
-        height: 40,
-        borderColor: "gray",
-        width: '100%',
-        borderWidth: 1,
-        borderRadius: 5,
-        paddingHorizontal: 10,
-        backgroundColor: "white",
-        marginBottom:10,
-    },
-    
-    select: {
-        paddingHorizontal: 10,
-        width: 200,
-    },
-    
-    button: {
-        backgroundColor: "#00B561",
-        borderRadius: 10,
-        padding: 15,
-        width: 280,
-        marginTop: 10,
-        marginBottom:10,
-    },
-    
-    inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    }
-    
+  header: {
+    padding: 16,
+    alignItems: "center",
+  },
+
+  user: {
+    textAlign: "left",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  img: {
+    height: 20,
+    width: 20,
+  },
+
+  title: {
+    textAlign: "left",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  contentContainer: {
+    paddingHorizontal: 16,
+  },
+
+  infos: {
+    marginTop: 28,
+    textAlign: "left",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  form: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+    paddingHorizontal: 16,
+  },
+
+  editNames: {
+    flexDirection: "row",
+    marginBottom: 16,
+    paddingHorizontal: 16,
+  },
+
+  editInfos: {
+    flexDirection: "row",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+
+  other: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 10,
+  },
+
+  names: {
+    height: 20,
+    borderColor: "gray",
+    width: 160,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
+    marginBottom: 10,
+  },
+
+  mdp: {
+    height: 20,
+    borderColor: "gray",
+    width: 160,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
+    marginBottom: 10,
+  },
+
+  input: {
+    height: 40,
+    borderColor: "gray",
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
+    marginBottom: 10,
+  },
+
+  select: {
+    paddingHorizontal: 10,
+    height: 20,
+    borderColor: "gray",
+    width: 160,
+    marginBottom: 10,
+  },
+
+  button: {
+    backgroundColor: "rgb(0, 181, 97)",
+    borderRadius: 10,
+    border: "none",
+    width: "100%",
+    marginTop: 20,
+    marginBottom: 10,
+  },
 });
-
-
-
--- views/UserProfile.tsx
+++ views/UserProfile.tsx
@@ -1,141 +1,143 @@
-import React, { useEffect, useState } from 'react';
-import { ImageBackground, View, Text, Image } from 'react-native';
-import {Button,Datepicker, IndexPath, Input, Select, SelectItem, SelectProps} from '@ui-kitten/components';
-import { styles } from '../Styles/UserProfile.style';
+import React, { useState } from "react";
+import { ImageBackground, View, Text, Image, ScrollView } from "react-native";
+import {
+  Button,
+  Datepicker,
+  IndexPath,
+  Input,
+  Select,
+  SelectItem,
+} from "@ui-kitten/components";
+import { styles } from "../Styles/UserProfile.style";
 
-interface Props {
-
-}
+interface Props {}
 
 export const UserProfile: React.FC<Props> = (props: Props) => {
-    const [firstname, setFirstname] = useState('');
-    const [lastname, setLastname] = useState('');
-    const [genre, setGenre] = useState('');
-    const [birthday, setBirthday] = useState('');
-    const [email, setEmail] = useState('');
-    const [alertMail, setAlertMail] = useState(false);
-    const [password, setPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [newPasswordConfirm, setNewPasswordConfirm] = useState('');
-    const [selectedIndex, setSelectedIndex] = React.useState<IndexPath | IndexPath[]>(new IndexPath(0));
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [selectedIndex, setSelectedIndex] = useState(new IndexPath(0));
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [email, setEmail] = useState("");
+  const [alertMail, setAlertMail] = useState(false);
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState("");
 
-    const [date, setDate] = React.useState(new Date());
-    const [selectedDate, setSelectedDate] = React.useState<Date | null>(null);
-    const onSave = async () => {
-        // Form validation
-        try{
-
-        } catch (error) {
-            // Handle registration error
-            console.log(error);
-        }
+  const onSave = async () => {
+    try {
+    } catch (error) {
+      console.log(error);
     }
-    const useSelectState = (initialState = undefined): SelectProps => {
-        const [selectedIndex, setSelectedIndex] = React.useState(initialState);
-        return { selectedIndex, onSelect: setSelectedIndex };
-    };
+  };
 
-    const genreSelectState = useSelectState();
-
-    return (
-    <View  style={styles.container}>
-        <ImageBackground source={require('../assets/backgrounds/aqua.png')} resizeMode="cover" style={styles.Background}>
-            <View style={styles.header}>
-                <Text style={styles.title}> Bienvenue </Text>
-                <Text style={styles.user}> Bobby singer  <Image style={styles.img} source={require('../assets/gold-logo.png')}/> </Text>    
-            </View>
-        </ImageBackground>
-        <Text style={styles.infos}> Vos informations </Text>
-            <View style={styles.form}>
-                <View style={styles.editNames}>
-                    <Input
-                        style={styles.names}
-                        value={firstname}
-                        onChangeText={setFirstname}
-                        placeholder="Nom"
-                    />
-
-                    {alertMail && <Text>Entrer un e-mail valide</Text>}
-
-                    <Input
-                        style={styles.names}
-                        value={lastname}
-                        onChangeText={setLastname}
-                        placeholder="Prenom"
-                    />
-                </View>
-               
-
-                <View style={styles.editInfos}>
-                    <Select
-                        selectedIndex={selectedIndex}
-                        onSelect={index => setSelectedIndex(index)}
-                        placeholder='Genre'
-                        {...genreSelectState}
-                        style={styles.select}
-                    >
-                        <SelectItem title='Homme' />
-                        <SelectItem title='Femme' />
-                    </Select>
-
-                    <Datepicker
-                         style={styles.select}
-                         placeholder='Date de naissance'
-                         date={selectedDate}
-                         onSelect={nextDate => setSelectedDate(nextDate)}
-                         // Efface la sélection en définissant selectedDate à null lorsque le composant est effacé
-                         onClear={() => setSelectedDate(null)}
-                    />
-                </View>
-
-                <View style={styles.other}>
-                    <Input
-                        style={styles.input}
-                        value={email}
-                        onChangeText={setEmail}
-                        placeholder="Email"
-                    />
-
-                    <Button style={styles.button} onPress={() => onSave()} >
-                        Enregistrer
-                    </Button>
-                </View>
-            </View>
-
-            <Text style={styles.infos}> Votre mot de passe </Text>
-            <View style={styles.form}>
-                <View style={styles.editNames}>
-                    <Input
-                            style={styles.names}
-                            value={password}
-                            onChangeText={setPassword}
-                            placeholder="Mot de passe actuel"
-                            secureTextEntry
-                        />
-
-                        <Input
-                            style={styles.names}
-                            value={newPassword}
-                            onChangeText={setNewPassword}
-                            placeholder="Nouveau mot de passe"
-                            secureTextEntry
-                        />
-
-                    </View>
-                    <View style={styles.other}>
-                        <Input
-                                style={styles.input}
-                                value={newPasswordConfirm}
-                                onChangeText={setNewPasswordConfirm}
-                                placeholder="Confirmation mot de passe"
-                                secureTextEntry
-                            />
-                    <Button style={styles.button} onPress={() => onSave()} >
-                        Enregistrer
-                    </Button>
-                </View>
-            </View>
-               
+  return (
+    <ScrollView style={styles.container}>
+      <ImageBackground
+        source={require("../assets/backgrounds/aqua.png")}
+        resizeMode="cover"
+        style={styles.background}
+      >
+        <View style={styles.header}>
+          <Text style={styles.title}>Bienvenue</Text>
+          <Text style={styles.user}>
+            Bobby singer{" "}
+            <Image
+              style={styles.img}
+              source={require("../assets/gold-logo.png")}
+            />
+          </Text>
         </View>
-  )
-}
+      </ImageBackground>
+
+      <View style={styles.contentContainer}>
+        <Text style={styles.infos}>Vos informations</Text>
+        <View style={styles.form}>
+          <View style={styles.editNames}>
+            <Input
+              style={styles.names}
+              value={firstname}
+              onChangeText={setFirstname}
+              placeholder="Nom"
+            />
+
+            {alertMail && <Text>Entrer un e-mail valide</Text>}
+
+            <Input
+              style={styles.names}
+              value={lastname}
+              onChangeText={setLastname}
+              placeholder="Prenom"
+            />
+          </View>
+
+          <View style={styles.editInfos}>
+            <Select
+              selectedIndex={selectedIndex}
+              onSelect={(index) => setSelectedIndex(index)}
+              placeholder="Genre"
+              style={styles.select}
+            >
+              <SelectItem title="Homme" />
+              <SelectItem title="Femme" />
+            </Select>
+
+            <Datepicker
+              style={styles.select}
+              placeholder="Date de naissance"
+              date={selectedDate}
+              onSelect={(nextDate) => setSelectedDate(nextDate)}
+              onClear={() => setSelectedDate(null)}
+            />
+          </View>
+
+          <View style={styles.other}>
+            <Input
+              style={styles.input}
+              value={email}
+              onChangeText={setEmail}
+              placeholder="Email"
+            />
+
+            <Button style={styles.button} onPress={onSave}>
+              Enregistrer
+            </Button>
+          </View>
+        </View>
+
+        <Text style={styles.infos}>Votre mot de passe</Text>
+        <View style={styles.form}>
+          <View style={styles.editNames}>
+            <Input
+              style={styles.mdp}
+              value={password}
+              onChangeText={setPassword}
+              placeholder="Actuel"
+              secureTextEntry
+            />
+
+            <Input
+              style={styles.mdp}
+              value={newPassword}
+              onChangeText={setNewPassword}
+              placeholder="Nouveau"
+              secureTextEntry
+            />
+          </View>
+
+          <View style={styles.other}>
+            <Input
+              style={styles.input}
+              value={newPasswordConfirm}
+              onChangeText={setNewPasswordConfirm}
+              placeholder="Confirmation"
+              secureTextEntry
+            />
+            <Button style={styles.button} onPress={onSave}>
+              Enregistrer
+            </Button>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
